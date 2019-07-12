ecb2fdbae82f09eda46677a99380ac1f2cd69a82
Hung Dang <hdang@athenahealth.com>
2019-07-12 06:40:35 -0400

    Add empty unittest files for hash functions and hash tables

---
+++ unittests/hash_function.cpp
@@ -0,0 +1,19 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+#include "fmt/format.h"
+#include "aquahash.h"
+#include <vector>
+#include <string>
+#include <tuple>
+
+TEST_CASE("Integral") {
+}
+
+TEST_CASE("Floating point") {
+}
+
+TEST_CASE("String") {
+}
+
+TEST_CASE("Array") {
+}
