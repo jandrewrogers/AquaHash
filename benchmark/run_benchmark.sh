5d9e12c9b1318b67c687a7efa12e3b01596642fa
Hung Dang <hdang@athenahealth.com>
2019-07-13 23:31:08 -0400

    Build all benchmark binaries before collecting the data.

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 N=4096
 mkdir -p data
+
+# Compile all benchmarks
+cmake ./
+make -j5
+
+# Run all benchmark and plot the collected data.
 for idx in `seq 1 1 $N`;
 do
     LEN=$idx ./random_string --benchmark_format=json --benchmark_out="data/data_$idx.json"
